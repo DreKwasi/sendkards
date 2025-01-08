3051b1614c060036ce478a7844b56ce6dd7c8148
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 15:34:24 +0000

    feat: added firebase credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  ssr: false,
+  pages: true,
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+    },
+  },
+  components: [{ path: "~/components", pathPrefix: false }],
+  runtimeConfig: {
+    public: {
+      apiKey: process.env.NUXT_APIKEY,
+      authDomain: process.env.NUXT_AUTHDOMAIN,
+      projectId: process.env.NUXT_PROJECTID,
+      storageBucket: process.env.NUXT_STORAGEBUCKET,
+      messagingSenderId: process.env.NUXT_MESSAGINGSENDERID,
+      appId: process.env.NUXT_APPID,
+      measurementId: process.env.NUXT_MEASUREMENTID,
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  colorMode: {
+    preference: "dark",
+    fallback: "light",
+    classPrefix: "",
+    classSuffix: "",
+    storage: "localStorage", // or 'sessionStorage' or 'cookie'
+    storageKey: "nuxt-color-mode",
+  },
+});
+
