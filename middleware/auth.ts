1cd33d7f4620daed9ba9c5a3b9ff9f60bf8fe2d1
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 17:49:39 +0000

    feat: middleware for handling auth

---
+++ middleware/auth.ts
@@ -0,0 +1,35 @@
+import { type User } from "firebase/auth";
+
+// middleware/auth.ts
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { getUser } = useFirebaseAuth();
+  const user = (await getUser()) as User;
+
+  // redirect the user to the login page
+  if (!user) {
+    return navigateTo({
+      path: "/",
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  }
+  if (!user.emailVerified) {
+    if (to.name !== "email-verification") {
+      return navigateTo({
+        path: "/email-verification",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    }
+  }
+  if (user.emailVerified && to.name === "email-verification") {
+    return navigateTo({
+      path: "/verify",
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  }
+});
