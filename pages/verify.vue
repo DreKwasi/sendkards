f4a8d0eaea8b18f5adc03730b26a10e259794cc1
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 23:11:06 +0000

    feat: initial setup for verify page

---
+++ pages/verify.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: "Verify Kard",
+    layout: "default",
+    name: "verify",
+    middleware: "auth",
+  });
+</script>
+<template>
+  <div class="grid place-items-center h-[90vh]">verify</div>
+</template>
