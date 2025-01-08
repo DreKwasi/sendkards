ce7f93a708c8445cd40af59970ecf6ae40dae0f4
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 17:51:22 +0000

    feat: setup for global state for user

---
+++ stores/userStore.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { type Auth, type User } from "firebase/auth";
+import type { UserDb } from "~/types/userTypes";
+
+export const useUserStore = defineStore("userStore", () => {
+  const { $auth } = useNuxtApp();
+  const auth = $auth as Auth;
+  const currentUser = auth.currentUser;
+  const userAuth = ref<User | null>(currentUser as User);
+  const isEmailVerified = ref<Boolean>(false);
+  const userRoles = ref<Record<string, string> | null>(null);
+  const isSuperAdmin = ref<Boolean>(false);
+  const userProfile = ref<UserDb | null>(null);
+
+  return { userAuth, isEmailVerified, userRoles, isSuperAdmin, userProfile };
+});
