e2d246557c8cefd3cb7427974d8d954eb729179b
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 23:17:43 +0000

    feat: added verification form

--- pages/verify.vue
+++ pages/verify.vue
@@ -5,7 +5,62 @@
     name: "verify",
     middleware: "auth",
   });
+  import { Button } from "@/components/ui/button";
+  import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+  import { Input } from "@/components/ui/input";
+  import { toTypedSchema } from "@vee-validate/zod";
+  import { useForm } from "vee-validate";
+  import * as z from "zod";
+
+  const formSchema = toTypedSchema(
+    z.object({
+      orderId: z.string().min(2).max(50),
+    })
+  );
+
+  const { handleSubmit } = useForm({
+    validationSchema: formSchema,
+  });
+
+  const onSubmit = handleSubmit((values) => {});
 </script>
 <template>
-  <div class="grid place-items-center h-[90vh]">verify</div>
+  <div class="grid place-items-center h-[90vh]">
+    <Card class="w-[350px]">
+      <CardHeader>
+        <CardTitle>Verify SendKard</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form class="w-full space-y-6" @submit="onSubmit">
+          <FormField v-slot="{ componentField }" name="orderId">
+            <FormItem>
+              <FormLabel>Enter Gift Card ID</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="1239801672" v-bind="componentField" />
+              </FormControl>
+              <FormDescription> Once you hit submit, we will verify the gift card </FormDescription>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button type="submit"> Submit </Button>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
 </template>
