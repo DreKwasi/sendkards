cc5b0f9fd49a5e7e6e28ee950b1b86dbc6ac7827
DreKwasi <andrewsboateng137@gmail.com>
2025-01-08 15:33:56 +0000

    feat: initial setup for home page

--- app.vue
+++ app.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+  import { Toaster } from "@/components/ui/sonner";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="font-geist">
+    <NuxtLayout>
+      <Toaster class="pointer-events-auto" :rich-colors="true" :close-button="true" />
+      <Spinner />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.4s;
+  }
+  .page-enter-from,
+  .page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+  }
+</style>
+
