f86b9121a0bce1a19661d5c8ad8a334974bc111c
DreKwasi <andrewsboateng137@gmail.com>
2025-01-09 00:41:12 +0000

    feat: composable for verifying send kard voucher

---
+++ composables/useVerifySendKard.ts
@@ -0,0 +1,21 @@
+import { logEvent, type Analytics } from "firebase/analytics";
+import { type Functions, httpsCallable } from "firebase/functions";
+
+export const useVerifySendKard = () => {
+  const { $analytics, $db, $functions } = useNuxtApp();
+  const functions = $functions as Functions;
+
+  const verifySendKard = async (voucherId: string) => {
+    try {
+      const verifySendKard = httpsCallable(functions, "verifySendKard");
+      const response = await verifySendKard({ voucherId: voucherId });
+      return response.data;
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+    }
+  };
+
+  return {
+    verifySendKard,
+  };
+};
