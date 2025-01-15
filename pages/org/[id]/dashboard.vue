be69248c6cf34fd0a3051a94de9810f54989c2ad
DreKwasi <andrewsboateng137@gmail.com>
2025-01-15 12:12:53 +0000

    feat: setup for dashboard page

---
+++ pages/org/[id]/dashboard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: "Dashboard",
+    layout: "auth",
+    name: "dashboard",
+    middleware: "auth",
+  });
+</script>
+<template>
+  <div class="grid place-items-center h-[90vh]"></div>
+</template>
