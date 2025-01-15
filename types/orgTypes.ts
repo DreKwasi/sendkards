a632e3c927f0b77f825441689252d25cda18580a
DreKwasi <andrewsboateng137@gmail.com>
2025-01-15 12:13:43 +0000

    feat: typings for organization data

---
+++ types/orgTypes.ts
@@ -0,0 +1,10 @@
+export type OrgType = {
+  id: string;
+  organization_name: string;
+  branch_name: string;
+  contact_email: string;
+  contact_info: string;
+  search_name_field: string;
+  location: string;
+  logo: string;
+};
