f98481aa97548b98c35d7905b61e1af8914cb957
DreKwasi <andrewsboateng137@gmail.com>
2025-01-15 12:11:16 +0000

    feat: created layout for auth views

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { SidebarProvider } from "~/components/ui/sidebar";
+</script>
+
+<template>
+  <div class="min-h-screen w-full max-w-screen-3xl md:mx-auto mt-10">
+    <SidebarProvider>
+      <AppSidebar />
+      <main class="w-full h-full px-4">
+        <slot />
+      </main>
+    </SidebarProvider>
+  </div>
+</template>
