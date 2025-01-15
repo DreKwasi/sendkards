773ddd3f0eb17902b97d6a8d5c0661e7659e78ee
DreKwasi <andrewsboateng137@gmail.com>
2025-01-15 12:13:03 +0000

    feat: setup for settings page

---
+++ pages/org/[id]/settings.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: "Settings",
+    layout: "auth",
+    name: "settings",
+    middleware: "auth",
+  });
+</script>
+<template>
+  <div class="grid place-items-center h-[90vh]"></div>
+</template>
