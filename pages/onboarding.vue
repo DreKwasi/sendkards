cd2b14161364388f064538e1bbfa3e9e7013070b
DreKwasi <andrewsboateng137@gmail.com>
2025-01-15 12:12:16 +0000

    feat: setup for onboarding page

---
+++ pages/onboarding.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: "Onboarding",
+    layout: "default",
+    name: "onboarding",
+    middleware: "auth",
+  });
+</script>
+<template>
+  <div class="grid place-items-center h-[90vh]"></div>
+</template>
